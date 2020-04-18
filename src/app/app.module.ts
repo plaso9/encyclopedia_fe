80b81a9fda4810c601c1a5aba8223878672caf17
plaso9 <piepoligiulio7@gmail.com>
2020-04-18 21:07:04 +0200

    Import SharedModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    SharedModule
   ],
   imports: [
     BrowserModule,
