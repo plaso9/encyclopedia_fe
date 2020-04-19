6e9ce788dbd6034e955b4d0d9b614738631e89ed
plaso9 <piepoligiulio7@gmail.com>
2020-04-20 00:42:42 +0200

    Set 100% height

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,8 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   // templateUrl: './app.component.html',
   template: `
-      <div class="container-fluid">
-        <div class="row flex-xl-nowrap">
+      <div class="container-fluid h-100">
+        <div class="row flex-xl-nowrap h-100">
         <div class="col-md-2 col-xl-2 bd-sidebar bg-light border-right"><app-sidebar></app-sidebar></div>
         <div class="col-md-10 col-xl-10 bd-sidebar bg-light border-right"><router-outlet></router-outlet></div>
         </div>
