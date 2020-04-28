564b35e835fc73bb85fbe98def1c9f8c4f49ecd4
plaso9 <piepoligiulio7@gmail.com>
2020-04-28 19:52:44 +0200

    Add ArgumentNew to router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './components/home/home.component';
 import { ArgumentComponent } from './components/argument/argument.component';
-
+import { ArgumentNewComponent } from './components/argument-new/argument-new.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: HomeComponent },
-  { path: 'argument', component: ArgumentComponent }
+  { path: 'argument', component: ArgumentComponent },
+  { path: 'argument-new', component: ArgumentNewComponent },
 ];
 
 @NgModule({
