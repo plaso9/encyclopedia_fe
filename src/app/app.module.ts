fce21c1dd8c0ad9f4bdd6ed405ca51521786d8ab
plaso9 <piepoligiulio7@gmail.com>
2020-05-26 08:38:55 +0200

    Import CommonsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { ComponentsModule } from './components/components.module';
+import { CommonsModule } from './commons/commons.module';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { ComponentsModule } from './components/components.module';
     AppRoutingModule,
     SharedModule,
     ComponentsModule,
+    CommonsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
