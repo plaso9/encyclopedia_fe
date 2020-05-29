773253f739f430ace346917d2ef00e1e84b039a5
plaso9 <piepoligiulio7@gmail.com>
2020-05-29 09:44:11 +0200

    Define Argument class

--- src/app/commons/argument/argument.service.ts
+++ src/app/commons/argument/argument.service.ts
@@ -1,14 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Argument } from './argument';
+import { ConstantsService } from '../constants/constants.service';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ArgumentService {
 
-  constructor() { }
+  constructor(private http: HttpClient, private _constant: ConstantsService) { }
 
   search(argumentJson){
-    console.log("Wei");
-    return argumentJson;
+    var urlParams = "";
+    if(argumentJson.argumentSearch) {
+      urlParams = "?s=true&descrizione=" +  argumentJson.argumentSearch;
+      this.http.get<Argument>(this._constant.baseApiUrl + 'argument' + urlParams).subscribe(data => {
+        console.log(data);
+      });
+      return argumentJson;
+    } else {
+      return;
+    }
   }
 }
--- src/app/commons/argument/argument.ts
+++ src/app/commons/argument/argument.ts
@@ -1,2 +1,11 @@
 export class Argument {
+  id: string;
+  title: string;
+  description: string;
+
+  constructor(argumentJson){
+    this.id = "not set";
+    this.title = argumentJson.argumentTitle;
+    this.description = argumentJson.argumentDescription;
+  }
 }
