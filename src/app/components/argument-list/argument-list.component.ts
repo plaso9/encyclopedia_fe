49934ef6909cc7b599b26817dc386124494cd714
plaso9 <piepoligiulio7@gmail.com>
2020-05-31 18:33:46 +0200

    Add search bar in ArgumentList

--- src/app/components/argument-list/argument-list.component.ts
+++ src/app/components/argument-list/argument-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators} from '@angular/forms';
+
 import { ArgumentService } from 'src/app/commons/argument/argument.service';
 import { Argument } from 'src/app/commons/argument/argument';
 
@@ -8,16 +10,24 @@ import { Argument } from 'src/app/commons/argument/argument';
   styleUrls: ['./argument-list.component.css']
 })
 export class ArgumentListComponent implements OnInit {
+  searchForm: FormGroup;
 
   //objects
   argumentObj: Argument;
 
-  constructor(private argument: ArgumentService) { }
+  constructor(private formbuilder: FormBuilder, private argument: ArgumentService) { }
 
   ngOnInit(): void {
+    this.searchForm = this.formbuilder.group({
+      argumentSearch: ['', [Validators.required]],
+    });
     this.argument.getAllArgument().subscribe(data => {
       this.argumentObj = data;
     });
+  }
+
+  onSubmit() {
+    this.argument.search(this.searchForm.value);
   }
 
 }
