a81df4ddf17824a251deea66ca594f3c79fe4a9c
plaso9 <piepoligiulio7@gmail.com>
2020-05-31 16:23:16 +0200

    Add router path for ArgumentList

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { ArgumentComponent } from './components/argument/argument.component';
 import { ArgumentNewComponent } from './components/argument-new/argument-new.component';
+import { ArgumentListComponent } from './components/argument-list/argument-list.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: HomeComponent },
   { path: 'argument', component: ArgumentComponent },
   { path: 'argument-new', component: ArgumentNewComponent },
+  { path: 'arguments', component: ArgumentListComponent },
 ];
 
 @NgModule({
